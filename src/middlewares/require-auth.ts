f3a9b0a52ae9a336d715d20a2fda5966f0a3caa3
Radu Hulubas <radu.hulubas@softvision.com>
2022-10-25 22:46:38 +0300

    Updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,13 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
+(JSON as any).safeStringify = (obj: any, indent = 2) => {
+  let cache :any= [];
+  const retVal = JSON.stringify(
+    obj,
+    (key, value) =>
+      typeof value === "object" && value !== null
+        ? cache.includes(value)
+          ? undefined // Duplicate reference found, discard key
+          : cache.push(value) && value // Store value in our collection
+        : value,
+    indent
+  );
+  cache = null;
+  return retVal;
+};
+
 export const requireAuth = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   if (!req.currentUser) {
-    console.log(' request is -> ', JSON.stringify(req));
+    console.log(' request is -> ', (JSON as any).safeStringify(req));
     throw new NotAuthorizedError();
   }
 
